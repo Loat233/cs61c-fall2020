edbcfa290621beb592cb3ef3eeb7b0efc4f8535b
Loat233 <a2847894378@outlook.com>
2026-03-23 14:12:25 +0800

    Finish lab2

--- lab/lab02/lfsr.c
+++ lab/lab02/lfsr.c
@@ -5,6 +5,6 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    *reg = (*reg >> 1) | ((((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1) << 15);
 }
 
